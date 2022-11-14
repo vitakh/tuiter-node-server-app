ab9517a3bfa005a4e98c0b298514275483d6487b
Vita Khan <vita.khan.2002@mail.ru>
2022-11-13 19:13:46 -0500

    first commit

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,44 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = new Date().getTime() + "";
+    newTuit.likes = 0;
+    newTuit.dislikes = 0;
+    newTuit.liked = false;
+    newTuit.disliked = false;
+    newTuit.userName = "NASA";
+    newTuit.handle = "@NASA";
+    newTuit.image = "nasa.png";
+    newTuit.time = "1s";
+    tuits.push(newTuit);
+    res.json(newTuit);
+};
+
+const findTuits  = (req, res) => {
+    res.json(tuits);
+}
+
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex((t) => t._id.toString() === tuitdIdToUpdate);
+    tuits[tuitIndex] =
+        {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+}
+
+const deleteTuit = (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    tuits = tuits.filter((t) =>
+                             t._id.toString() !== tuitdIdToDelete);
+    res.sendStatus(200);
+}
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
